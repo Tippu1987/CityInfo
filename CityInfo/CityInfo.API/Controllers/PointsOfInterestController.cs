54db222d37db5cc1ed166ae797bd0adf5d24bd7f
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-09-29 22:04:18 -0700

    Module 4--Manipulating Resources--Done

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,5 +1,6 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
@@ -20,8 +21,8 @@ namespace CityInfo.API.Controllers
         }
 
         [HttpGet]
-        [Route("{id}")]
-        public IActionResult getpointOfInterest(int cityId, int id)
+        [Route("{id}",Name ="RouteParam")]
+        public IActionResult GetpointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
             if (city == null)
@@ -34,6 +35,55 @@ namespace CityInfo.API.Controllers
                 else
                     return Ok(poi);
             }
+        }
+
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId, PointsOfInterestForCreationDTO pointOfInterest)
+        {
+            //For any complex validations like checking if name==description or if same POI is added earlier..we can check Fluent Validation here..
+            //https://fluentvalidation.net/
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+                return NotFound();
+            int nextId = city.PointsOfInterest.Max(x => x.Id) + 1;
+            PointsOfInterestDTO finalpoi = new PointsOfInterestDTO
+            {
+                Id = nextId,
+                Description = pointOfInterest.Description,
+                Name = pointOfInterest.Name
+            };
+            city.PointsOfInterest.Add(finalpoi);
+
+            return CreatedAtRoute("RouteParam",
+                new { cityId, finalpoi.Id },
+                finalpoi);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePointOfInterest(PointsOfInterestForUpdateDTO pointsOfInterest, int cityId, int id)
+        {
+            if (pointsOfInterest.Name == pointsOfInterest.Description)
+                ModelState.AddModelError("Description", "Name and Description cannot be the Same");
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            var poi = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+            if (city == null || poi == null)
+                return NotFound();
+            poi.Name = pointsOfInterest.Name;
+            poi.Description = pointsOfInterest.Description;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            var poi = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+            if (city == null || poi == null)
+                return BadRequest();
+            city.PointsOfInterest.Remove(poi);
+            return NoContent();
         }
     }
 }
---
+++ CityInfo/CityInfo.API/Models/PointsOfInterestForCreationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointsOfInterestForCreationDTO
+    {
+        [Required(AllowEmptyStrings = false,ErrorMessage ="You should provide a description for Name Property")]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
